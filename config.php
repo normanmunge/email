f02bb2ed3a5e48594c21baea33a8844b6918f8b1
normanmunge <normanmunge@gmail.com>
2015-09-02 14:57:37 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Email
+|--------------------------------------------------------------------------
+|
+| Email Preferences
+|
+*/
+$config['useragent'] = 'CodeIgniter';
+$config['protocol'] = 'smtp';
+$config['smtp_host']    = 'ssl://smtp.gmail.com';
+$config['smtp_port']    = '465';
+$config['smtp_timeout'] = '7';
+$config['smtp_user']    = 'normanmunge2014@gmail.com';
+$config['smtp_pass']    = 'peteresther';
+$config['wordwrap'] = TRUE;
+$config['wrapchars'] = 1000;
+$config['mailtype'] = 'html';
+$config['charset']    = 'utf-8';
+$config['newline']    = "\r\n";
+$config['mailtype'] = 'text'; // or html
+$config['validation'] = TRUE; // bool whether to validate email or not
+
